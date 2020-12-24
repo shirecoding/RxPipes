cf369db348bbfccec634b57ffce52857fc960c2e
Benjamin Hon <benjaminhon86@gmail.com>
2020-12-24 12:52:30 +0800

    update readme

--- setup.py
+++ setup.py
@@ -6,12 +6,20 @@ setup(
     name='rxpipes',
     version=__version__,
     author='shirecoding',
+    author_email="shirecoding@gmail.com",
     install_requires=[
         'rx',
         'toolz'
     ],
     url='https://github.com/shirecoding/RxPipes',
+    download_url=f'https://github.com/shirecoding/ModelMaker/archive/{__version__}.tar.gz',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     zip_safe=False,
     include_package_data=True,
     packages=find_packages() + [],
